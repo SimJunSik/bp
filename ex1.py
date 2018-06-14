82bfdca3316ab863f0cb7f176e8400ecd331901d
SimJunSik <wnstlr24@naver.com>
2018-06-15 00:17:48 +0900

    06151217

--- ex1.py
+++ ex1.py
@@ -27,17 +27,20 @@ keep_prob = tf.placeholder(tf.float32)
 W1 = tf.Variable(tf.random_normal([3, 3, channel, 32], stddev=0.01))
 L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
 L1 = tf.nn.relu(L1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
 L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
 L2 = tf.nn.relu(L2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
 L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
 L3 = tf.nn.relu(L3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
